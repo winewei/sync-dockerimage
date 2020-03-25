0fd45ff31642070afbda4ab709c4ecc581037d8a
winewei <winewei@yeah.net>
2020-03-25 22:58:12 +0800

    update docker registry region

--- push-image.sh
+++ push-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ALI_REGISTRY=registry.cn-hangzhou.aliyuncs.com/sync-dockerimage
+ALI_REGISTRY=registry.cn-shenzhen.aliyuncs.com/sync-dockerimage
 
 #config the following env at travis-ui
 docker login registry.cn-shenzhen.aliyuncs.com -u $DOCKER_USER -p $DOCKER_PASSWORD
