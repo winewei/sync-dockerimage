29e0d62f00281828e72208add604aac27a1b6af4
winewei <winewei@yeah.net>
2020-04-22 21:16:57 +0800

    update images list

--- push-image.sh
+++ push-image.sh
@@ -5,9 +5,9 @@ ALI_REGISTRY=registry.cn-hongkong.aliyuncs.com/sync-dockerimage
 #config the following env at travis-ui
 docker login registry.cn-hongkong.aliyuncs.com -u $DOCKER_USER -p $DOCKER_PASSWORD
 
-for image in $(cat img-list.txt)
+for image in $(cat img-list.txt | grep -v '^#')
 do
-	imagename=$(echo $image | awk -F '/' '{print $NF}')
+	imagename=$(echo $image | grep -v '^#' | awk -F '/' '{print $NF}')
 	docker pull $image
 	docker tag $image $ALI_REGISTRY/$imagename
 	# push到阿里云仓库
